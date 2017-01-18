2e6125d7f68bbe8653780d595c9d7745afbe63b2
Martin Kielhorn <kielhorn.martin@gmail.com>
2017-01-18 11:54:48 +0100

    cmake

--- gen.lisp
+++ gen.lisp
@@ -6,10 +6,10 @@
 
 
 (defmacro with-glfw-window ((win &key (w 512) (h 512) (title "glfw")) &body body)
-  `(let ((,win :type GLFWwindow))
+  `(let ((,win :type GLFWwindow*))
      (if (! (funcall glfwInit))
 	 (statements (return -1)))
-     (setf ,win (funcall glfwCreateWindow ,w ,h ,title NULL NULL))
+     (setf ,win (funcall glfwCreateWindow ,w ,h (string ,title) NULL NULL))
      (if (! ,win)
 	 (statements (funcall glfwTerminate)
 		     (return -1)))
@@ -18,29 +18,36 @@
      (funcall glfwTerminate)))
 
 
-(defmacro with-c-file ((f fn) &body body)
-  `(let ((,f :type FILE :init (funcall fopen ,fn)
-	   ))
-     ,@body))
 
-(with-output-to-string (s)
-  (emit-cpp
-   :str s
-   :clear-env t
+
+(progn
+  (defparameter *out-cpp-filename*  (merge-pathnames "stage/gen-glfw/source/main.cpp"
+						   (user-homedir-pathname)))
+  
+  (with-open-file (s *out-cpp-filename*
+		    :direction :output
+		    :if-exists :supersede
+		    :if-does-not-exist :create)
+					;with-output-to-string (s)
+   (emit-cpp
+    :str s
+    :clear-env t
    
-   :code 
-   `(with-compilation-unit
-	(include "GLFW/glfw3.h")
-      (function (main ((argc :type int)
-		       (argv :type char**))
-		      int)
-		(funcall printf "blb")
-		
-		(macroexpand
-		 (with-glfw-window (w)
-		   (funcall printf "blb")))
-		(return 0))
-      )))
+    :code 
+    `(with-compilation-unit
+	 (include "GLFW/glfw3.h")
+       (function (main ((argc :type int)
+			(argv :type char**))
+		       int)
+		 (macroexpand
+		  (with-glfw-window (main_window)
+		    (for (() (! (funcall glfwWindowShouldClose main_window)) ())
+			 (funcall glClear GL_COLOR_BUFFER_BIT)
+			 (funcall glfwSwapBuffers main_window)
+			 (funcall glfwPollEvents))))
+		 (return 0))
+       )))
+  (sb-ext:run-program "/usr/bin/clang-format" (list "-i" (namestring *out-cpp-filename*))))
 
 
 
