0240ac84672d1396b7192e0bab6f1c9353cfdfb4
Martin Kielhorn <kielhorn.martin@gmail.com>
2017-01-18 15:06:04 +0100

    add library code

--- gen.lisp
+++ gen.lisp
@@ -25,14 +25,79 @@
 
 
 
-
+;; http://nullprogram.com/blog/2015/06/06/
+;; https://bitbucket.org/alfonse/glloadgen/wiki/Home
+;; http://nullprogram.com/blog/2014/12/23/ Interactive Programming in C
 
 
 (progn
-  (defparameter *out-cpp-filename*  (merge-pathnames "stage/gen-glfw/source/main.cpp"
+  (defparameter *lib-h-filename*  (merge-pathnames "stage/gen-glfw/source/lib.h"
+						     (user-homedir-pathname)))
+  (defparameter *lib-cpp-filename*  (merge-pathnames "stage/gen-glfw/source/lib.cpp"
 						   (user-homedir-pathname)))
   
-  (with-open-file (s *out-cpp-filename*
+  (with-open-file (s *lib-h-filename*
+		    :direction :output
+		    :if-exists :supersede
+		    :if-does-not-exist :create)
+    (emit-cpp
+    :str s
+    :clear-env t
+    :code 
+    `(with-compilation-unit
+	 (raw "#pragma once")
+       (statements (raw "struct lib_state"))
+       (struct lib_api ()
+	       (function ("(*init)" () "struct lib_state*"))
+	       (function ("(*finalize)" ((state :type "struct lib_state*")) void))
+	       (function ("(*reload)" ((state :type "struct lib_state*")) void))
+	       (function ("(*unload)" ((state :type "struct lib_state*")) void))
+	       (function ("(*step)" ((state :type "struct lib_state*")) int))
+	       )
+       (decl ((LIB_API :type "extern const struct lib_api"))))))
+
+  (with-open-file (s *lib-cpp-filename*
+		    :direction :output
+		    :if-exists :supersede
+		    :if-does-not-exist :create)
+   (emit-cpp
+    :str s
+    :clear-env t
+    :code 
+    `(with-compilation-unit
+	 (include "lib.h")
+       (include "stdlib.h")
+       (struct lib_state ()
+	       (decl ((r :type float))))
+       (function (lib_init () "static struct lib_state*")
+		 (let ((state :type "struct lib_state*"
+			      :init (funcall malloc (funcall sizeof *state))))
+		   (return state)))
+       (function (lib_reload ((state :type "struct lib_state*")) "static void")
+		 (raw "//"))
+       (function (lib_unload ((state :type "struct lib_state*")) "static void")
+		 (raw "//"))
+
+       (function (lib_finalize ((state :type "struct lib_state*")) "static void")
+		 (funcall free state))
+       (function (lib_step ((state :type "struct lib_state*")) "static int")
+		 (return 1))
+       (decl ((GAME_API :type "const struct lib_api" :init
+			(list lib_init
+			      lib_finalize
+			      lib_reload
+			      lib_unload
+			      lib_step))))
+       )))
+  (sb-ext:run-program "/usr/bin/clang-format" (list "-i" (namestring *lib-cpp-filename*)))
+  (sb-ext:run-program "/usr/bin/clang-format" (list "-i" (namestring *lib-h-filename*))))
+
+
+(progn
+  (defparameter *main-cpp-filename*  (merge-pathnames "stage/gen-glfw/source/main.cpp"
+						   (user-homedir-pathname)))
+  
+  (with-open-file (s *main-cpp-filename*
 		    :direction :output
 		    :if-exists :supersede
 		    :if-does-not-exist :create)
@@ -74,7 +139,9 @@
 			 (funcall glfwPollEvents))))
 		 (return 0))
        )))
-  (sb-ext:run-program "/usr/bin/clang-format" (list "-i" (namestring *out-cpp-filename*))))
+  (sb-ext:run-program "/usr/bin/clang-format" (list "-i" (namestring *main-cpp-filename*))))
+
+
 
 
 
